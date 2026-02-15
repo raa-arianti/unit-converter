1fc99e4a2b541e4e9644ce6a81235980d4182648
Ira Arianti Alawiah <ira.arianti@ui.ac.id>
2026-02-15 11:25:49 +0700

    Update Time.h

--- include/Time.h
+++ include/Time.h
@@ -1,6 +1,14 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include "Converter.h"
+#include <string>
+
 class Time : public Converter {
 public:
     double convert(double value,
                    const std::string& from,
                    const std::string& to) override;
 };
+
+#endif
